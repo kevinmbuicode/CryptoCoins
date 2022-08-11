d9c1de65506b5f67cd94cd6aeb83e9732eeb7188
kevinmbuicode <kevinmushan@gmail.com>
2022-08-11 16:12:41 +0300

    Context API set, working

---
+++ crypto-watch/src/CryptoContext.js
@@ -0,0 +1,32 @@
+import React, { useContext, useEffect, useState } from "react";
+import { createContext } from "react";
+
+const Crypto = createContext();
+
+const CryptoContext = ({ children }) => {
+  const [currency, setCurrency] = useState("USD");
+  const [symbol, setSymbol] = useState("KSH");
+
+  useEffect(()=> {
+    if (currency === "KSH"){
+        console.log("KSH");
+        setSymbol("KSH");
+    }
+    else if(currency === "USD"){
+        console.log("USD");
+        setSymbol("$");
+    }
+  }, [currency])
+
+  return (
+    <Crypto.Provider value={{ currency, symbol, setCurrency }}>
+      {children}
+    </Crypto.Provider>
+  );
+};
+
+export default CryptoContext;
+
+export const CryptoState = () => {
+  return useContext(Crypto);
+};
--- crypto-watch/src/components/Header.js
+++ crypto-watch/src/components/Header.js
@@ -3,9 +3,11 @@ import { Container, ThemeProvider } from "@mui/system";
 import React from "react";
 import "./Header.css";
 import { useNavigate } from "react-router-dom";
+import {CryptoState} from '../CryptoContext';
 
 const Header = () => {
   const navigate = useNavigate();
+  const {currency, symbol, setCurrency} = CryptoState();
 
   const darkTheme = createTheme({
     palette: {
@@ -21,6 +23,7 @@ const Header = () => {
           <Typography
             onClick={() => navigate("/")}
             className="Header-Typography"
+            variant="h6"
           >
             Crypto Watcher
           </Typography>
@@ -29,8 +32,10 @@ const Header = () => {
             style={{
               width: "100px",
               height: 40,
-              marginLeft: 15,
+              marginRight: 15,
             }}
+            value={currency}
+            onChange={(e) => setCurrency(e.target.value)}
           >
             <MenuItem value={"USD"}>USD</MenuItem>
             <MenuItem value={"KSH"}>KSH</MenuItem>
